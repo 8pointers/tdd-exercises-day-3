1b17c9c108254207b4ef2d2daf370c64afd5710b
Damjan Vujnovic <damjan@samuraiprinciple.com>
2020-11-29 18:30:00 +0000

    update

--- ex4/dont-peek/GameOfLife.cs
+++ ex4/dont-peek/GameOfLife.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace lifeSolution
@@ -6,29 +7,27 @@ namespace lifeSolution
     public class GameOfLife
     {
         private static readonly (int OffsetRow, int OffsetColumn)[] Deltas = {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)};
-        private Dictionary<(int Row, int Column), bool> _isAlive = new Dictionary<(int row, int column), bool>();
+        private HashSet<(int Row, int Column)> _isAlive = new HashSet<(int row, int column)>();
 
         public bool IsCellAlive(int row, int column)
         {
-            return _isAlive.ContainsKey((row, column));
+            return _isAlive.Contains((row, column));
         }
 
         public GameOfLife ToggleCellState(int row, int column)
         {
-            if (_isAlive.ContainsKey((row, column)))
-                _isAlive.Remove((row, column));
-            else
-                _isAlive[(row, column)] = true;
+            (_isAlive.Contains((row, column)) ? (Func<(int, int), bool>) _isAlive.Remove : _isAlive.Add)((row, column));
             return this;
         }
 
         public void Tick()
         {
-            _isAlive = _isAlive.Keys
-                .SelectMany(cell => Deltas.Select(delta => (Row: cell.Row + delta.OffsetRow, Column: cell.Column + delta.OffsetColumn)))
+            _isAlive = _isAlive
+                .SelectMany((cell, _) => Deltas.Select(delta => (Row: cell.Row + delta.OffsetRow, Column: cell.Column + delta.OffsetColumn)))
                 .GroupBy(cell => cell, (cell, cells) => new {Cell = cell, NumberOfLiveNeighbours = cells.Count()})
                 .Where(candidate => IsCellAlive(candidate.Cell.Row, candidate.Cell.Column) && candidate.NumberOfLiveNeighbours == 2 || candidate.NumberOfLiveNeighbours == 3)
-                .ToDictionary(aliveOnes => aliveOnes.Cell, _ => true);
+                .Select(candidate => candidate.Cell)
+                .ToHashSet();
         }
     }
 }
